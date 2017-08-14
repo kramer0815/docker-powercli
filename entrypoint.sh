6145fa3210f85371a79a7f394ec9ce329d085ef0
kramsen <kramer0815@users.noreply.github.com>
2017-08-14 11:00:16 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 yum -y install https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.14/powershell-6.0.0_alpha.14-1.el7.centos.x86_64.rpm
+
+if [[ -z ${1} ]]; then
+  exec $(which powershell)
+else
+  exec "$@"
+fi
